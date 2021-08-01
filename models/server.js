e5b95306e98313f5900bf0b76e2fd1c1fca3adbd
SilviuDN <silviudilimot@gmail.com>
2021-08-01 03:15:01 +0200

    Cors configurated

--- models/server.js
+++ models/server.js
@@ -14,7 +14,10 @@ class Server{
 
         this.server = http.createServer(this.app)
 
-        this.io = socketio(this.server, {})
+        this.io = socketio(this.server, {cors: {
+            origin: "*",
+            methods: ["GET", "POST"]
+          }})
     }
 
     middlewares(){

1238f8103bbac04d52e742b20398ed9b3cfc4686
SilviuDN <silviudilimot@gmail.com>
2021-08-01 03:03:54 +0200

    Cors updated

--- models/server.js
+++ models/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
 const path = require('path')
+const cors = require('cors')
 
 const  Sockets = require('./sockets')
 
@@ -18,6 +19,7 @@ class Server{
 
     middlewares(){
         this.app.use( express.static(path.resolve(__dirname,'../public')) )
+        this.app.use(cors())
     }
 
     configSockets(){
